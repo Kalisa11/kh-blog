23c29e97b74ec913b7365c13dad2f372bb228444
Kalisa <honorekals5@gmail.com>
2022-09-10 15:48:48 +0200

    Related Posts section

--- services/index.js
+++ services/index.js
@@ -1,6 +1,7 @@
-import { request, gql } from "graphql-request";
+import { request, gql, GraphQLClient } from "graphql-request";
 
 const graphqlAPI = process.env.GRAPHKHS_ENPOINT;
+const graphQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_ENDPOINT);
 
 export const getPosts = async () => {
   const query = gql`
@@ -34,4 +35,47 @@ export const getPosts = async () => {
   `;
   const result = await request(graphqlAPI, query);
   return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await graphQLClient.request(query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+  return result.posts;
 };
